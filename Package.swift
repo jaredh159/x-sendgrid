54db08703fa0bd32936dabe82b5073e294792660
Jared Henderson <jaredh159@users.noreply.github.com>
2022-06-22 15:40:29 -0400

    fix attachments (#1)

--- Package.swift
+++ Package.swift
@@ -9,14 +9,12 @@ let package = Package(
     .library(name: "XSendGrid", targets: ["XSendGrid"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/jaredh159/x-kit.git", from: "1.0.0"),
     .package(url: "https://github.com/jaredh159/x-http.git", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-nonempty.git", from: "0.3.0"),
   ],
   targets: [
     .target(name: "XSendGrid", dependencies: [
       .product(name: "XHttp", package: "x-http"),
-      .product(name: "XBase64", package: "x-kit"),
       .product(name: "NonEmpty", package: "swift-nonempty"),
     ]),
   ]
